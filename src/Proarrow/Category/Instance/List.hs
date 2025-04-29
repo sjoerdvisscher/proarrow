c66a35d5c1d6b882830a7ec4409f84c278fe9ae9
Sjoerd Visscher <sjoerd@w3future.com>
2025-04-29 20:03:23 +0200

    Strictified with shallow singletons

--- src/Proarrow/Category/Instance/List.hs
+++ src/Proarrow/Category/Instance/List.hs
@@ -3,11 +3,9 @@
 
 module Proarrow.Category.Instance.List where
 
-import Prelude (type (~))
-
 import Proarrow.Category.Monoidal (Monoidal (..), MonoidalProfunctor (..))
-import Proarrow.Category.Monoidal.Strictified (type (++))
-import Proarrow.Core (CAT, CategoryOf (..), Is, Profunctor (..), Promonad (..), UN, dimapDefault)
+import Proarrow.Category.Monoidal.Strictified qualified as Str
+import Proarrow.Core (CAT, CategoryOf (..), Is, Profunctor (..), Promonad (..), UN, dimapDefault, obj)
 
 type data LIST k = L [k]
 type instance UN L (L as) = as
@@ -17,36 +15,28 @@ type List :: CAT (LIST k)
 -- | The free monoid in CAT
 data List as bs where
   Nil :: List (L '[]) (L '[])
-  Cons :: (IsList as, IsList bs) => a ~> b -> List (L as) (L bs) -> List (L (a ': as)) (L (b ': bs))
+  Cons :: (Str.IsList as, Str.IsList bs) => a ~> b -> List (L as) (L bs) -> List (L (a ': as)) (L (b ': bs))
 
 mkCons :: (CategoryOf k) => (a :: k) ~> b -> L as ~> L bs -> L (a ': as) ~> L (b ': bs)
 mkCons f fs = Cons f fs \\ fs
 
-class ((as ++ bs) ++ cs ~ as ++ (bs ++ cs)) => Assoc as bs cs
-instance (as ++ (bs ++ cs) ~ (as ++ bs) ++ cs) => Assoc as bs cs
-class (as ~ as ++ '[], forall bs cs. Assoc as bs cs) => IsList as where
-  listId :: List (L as) (L as)
-  withIsList2 :: IsList bs => (IsList (as ++ bs) => r) -> r
-instance IsList '[] where
-  listId = Nil
-  withIsList2 r = r
-instance (CategoryOf k, Ob (a :: k), IsList as) => IsList (a ': as) where
-  listId = Cons id listId
-  withIsList2 @bs r = withIsList2 @as @bs r
-
 instance (CategoryOf k) => CategoryOf (LIST k) where
   type (~>) = List
-  type Ob as = (Is L as, IsList (UN L as))
+  type Ob as = (Is L as, Str.IsList (UN L as))
 
 instance (CategoryOf k) => Promonad (List :: CAT (LIST k)) where
-  id = listId
+  id @(L bs) = case Str.sList @bs of
+    Str.Nil -> Nil
+    Str.Sing @a -> Cons (obj @a) Nil
+    Str.Cons @a @as -> Cons (obj @a) (obj @(L as))
   Nil . Nil = Nil
   Cons f fs . Cons g gs = Cons (f . g) (fs . gs)
 
 instance (CategoryOf k) => Profunctor (List :: CAT (LIST k)) where
   dimap = dimapDefault
   r \\ Nil = r
-  r \\ Cons f fs = r \\ f \\ fs
+  r \\ Cons f Nil = r \\ f
+  r \\ Cons f fs@Cons{} = r \\ f \\ fs
 
 instance (CategoryOf k) => MonoidalProfunctor (List :: CAT (LIST k)) where
   par0 = Nil
@@ -57,8 +47,8 @@ instance (CategoryOf k) => MonoidalProfunctor (List :: CAT (LIST k)) where
 
 instance (CategoryOf k) => Monoidal (LIST k) where
   type Unit = L '[]
-  type p ** q = L (UN L p ++ UN L q)
-  withOb2 @(L as) @(L bs) = withIsList2 @as @bs
+  type p ** q = L (UN L p Str.++ UN L q)
+  withOb2 @(L as) @(L bs) = Str.withIsList2 @as @bs
   leftUnitor = id
   leftUnitorInv = id
   rightUnitor = id
