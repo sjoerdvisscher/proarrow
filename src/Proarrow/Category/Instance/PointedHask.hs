ce0c5497a09a6f0577a1c5cc28d255155d9d66ea
Sjoerd Visscher <sjoerd@w3future.com>
2025-05-23 16:05:15 +0200

    Fix Pointed

--- src/Proarrow/Category/Instance/PointedHask.hs
+++ src/Proarrow/Category/Instance/PointedHask.hs
@@ -1,32 +1,36 @@
 module Proarrow.Category.Instance.PointedHask where
 
 import Data.Kind (Type)
-import Prelude (Maybe (..), type (~), const, maybe, (>>=), (++))
+import Prelude (Maybe (..), type (~), const, ($), (>>=), (++))
 
 import Proarrow.Core (CAT, CategoryOf (..), Profunctor (..), Promonad (..), UN, dimapDefault)
 import Proarrow.Object.BinaryProduct (HasBinaryProducts(..))
 import Proarrow.Object.Terminal (HasTerminalObject(..))
 import Data.Void (Void, absurd)
-import Proarrow.Category.Monoidal (Monoidal(..), MonoidalProfunctor (..))
+import Proarrow.Category.Monoidal (Monoidal(..), MonoidalProfunctor (..), SymMonoidal (..))
 import Proarrow.Object.BinaryCoproduct (HasBinaryCoproducts (..))
 import Proarrow.Object.Initial (HasInitialObject (..))
-import Proarrow.Functor (map)
 import Proarrow.Category.Monoidal.Applicative (liftA2)
 import Proarrow.Monoid (Monoid (..), Comonoid (..))
+import Proarrow.Object.Exponential (Closed (..))
 
 type data POINTED = P Type
 type instance UN P (P a) = a
 
+-- | The category of types with an added point and point-preserving morphisms.
 type Pointed :: CAT POINTED
 data Pointed a b where
-  P :: (Maybe a -> Maybe b) -> Pointed (P a) (P b)
+  P :: (a -> Maybe b) -> Pointed (P a) (P b)
+
+toHask :: P a ~> P b -> (Maybe a -> Maybe b)
+toHask (P f) = (>>= f)
 
 instance Profunctor Pointed where
   dimap = dimapDefault
   r \\ P{} = r
 instance Promonad Pointed where
-  id = P id
-  P f . P g = P (f . g)
+  id = P Just
+  P f . P g = P (\a -> g a >>= f)
 instance CategoryOf POINTED where
   type (~>) = Pointed
   type Ob a = (a ~ P (UN P a))
@@ -35,8 +39,8 @@ data These a b = This a | That b | These a b
 instance HasBinaryProducts POINTED where
   type P a && P b = P (These a b)
   withObProd r = r
-  fst = P (\case Nothing -> Nothing; Just (This a) -> Just a; Just (That _) -> Nothing; Just (These a _) -> Just a)
-  snd = P (\case Nothing -> Nothing; Just (This _) -> Nothing; Just (That b) -> Just b; Just (These _ b) -> Just b)
+  fst = P (\case This a -> Just a; That _ -> Nothing; These a _ -> Just a)
+  snd = P (\case This _ -> Nothing; That b -> Just b; These _ b -> Just b)
   P f &&& P g = P (\a -> case (f a, g a) of
     (Just a', Just b') -> Just (These a' b')
     (Just a', Nothing) -> Just (This a')
@@ -49,48 +53,56 @@ instance HasTerminalObject POINTED where
 instance HasBinaryCoproducts POINTED where
   type P a || P b = P (a || b)
   withObCoprod r = r
-  lft = P (map lft)
-  rgt = P (map rgt)
-  P f ||| P g = P (>>= (f . Just ||| g . Just))
+  lft = P (Just . lft)
+  rgt = P (Just . rgt)
+  P f ||| P g = P (f ||| g)
 instance HasInitialObject POINTED where
   type InitialObject = P Void
-  initiate = P (maybe Nothing absurd)
+  initiate = P absurd
 
 instance MonoidalProfunctor Pointed where
-  par0 = P id
-  P f `par` P g = P (\case Nothing -> liftA2 id (f Nothing, g Nothing); Just (a, b) -> liftA2 id (f (Just a), g (Just b)))
+  par0 = P Just
+  P f `par` P g = P (\(a, b) -> liftA2 id (f a, g b))
 -- | The smash product of pointed sets.
 -- Monoids relative to the smash product are absorption monoids.
 instance Monoidal POINTED where
   type Unit = P ()
   type P a ** P b = P (a, b)
   withOb2 r = r
-  leftUnitor = P (map snd)
-  leftUnitorInv = P (map ((),))
-  rightUnitor = P (map fst)
-  rightUnitorInv = P (map (,()))
-  associator = P (\case
-    Nothing -> Nothing
-    Just ((a, b), c) -> Just (a, (b, c)))
-  associatorInv = P (\case
-    Nothing -> Nothing
-    Just (a, (b, c)) -> Just ((a, b), c))
-
--- TODO Pointed is closed: https://ncatlab.org/nlab/show/pointed+object#ClosedMonoidalStructure
-
--- | Conjunction with False = Nothing, True = Just ()
-instance Monoid (P ()) where
-  mempty = P (const (Just ()))
-  mappend = P (map fst)
+  leftUnitor = P (Just . snd)
+  leftUnitorInv = P (Just . ((),))
+  rightUnitor = P (Just . fst)
+  rightUnitorInv = P (Just . (,()))
+  associator = P (\((a, b), c) -> Just (a, (b, c)))
+  associatorInv = P (\(a, (b, c)) -> Just ((a, b), c))
+instance SymMonoidal POINTED where
+  swap = P (Just . swap)
+instance Closed POINTED where
+  type P a ~~> P b = P (a -> Maybe b)
+  withObExp r = r
+  curry (P f) = P (\a -> Just (\b -> f (a, b)))
+  uncurry (P f) = P (\(a, b) -> f a >>= ($ b))
 
 instance Monoid (P Void) where
   mempty = P (const Nothing)
-  mappend = P (map fst)
+  mappend = P (Just . fst)
+
+-- | Lift Hask monoids.
+memptyDefault :: Monoid a => Unit ~> P a
+memptyDefault = P (Just . mempty)
+
+mappendDefault :: Monoid a => P a ** P a ~> P a
+mappendDefault = P (Just . mappend)
+
+-- | Conjunction with False = Nothing, True = Just ()
+instance Monoid (P ()) where
+  mempty = memptyDefault
+  mappend = mappendDefault
 
 instance Monoid (P [a]) where
-  mempty = P (map (const []))
-  mappend = P (map (\(a, b) -> a ++ b))
+  mempty = memptyDefault
+  mappend = mappendDefault
 
 instance Comonoid (P x) where
-  counit = P (map (const ()))
-  comult = P (map (\x -> (x, x)))
+  counit = P (Just . counit)
+  comult = P (Just . comult)
